cb50dc16442c953ac6ebf816548dcdfee477adad
ilyes <ilyesben2009@hotmail.com>
2023-06-11 20:42:00 +0100

    first

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import './styles.css';
+import portugalFlagImage from './images/illustration-portugal-flag_53876-18170.jpg.avif';
+import TopRestaurantsPage from './components/TopRestaurants/TopRestaurantsPage';
+import HomePage from './components/HomePage';
+import CuisinesPage from './components/cuisines/CuisinesPage';
+import MapPage from './components/map/MapPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <header>
+          <img src={portugalFlagImage} alt="Portugal Flag" />
+          <h1>Lisbon Restaurant Recommendations</h1>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/TopRestaurants">Top Restaurants</Link>
+            <Link to="/cuisines">Cuisines</Link>
+            <Link to="/map">Map</Link>
+          </nav>
+        </header>
+        <Routes>
+          <Route path="/" exact element={<HomePage />} />
+          <Route path="/TopRestaurants" exact element={<TopRestaurantsPage />} />
+          <Route path="/cuisines" exact element={<CuisinesPage />} />
+          <Route path="/map" exact element={<MapPage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
